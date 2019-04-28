3e49496231297a792d07b565df68b825b8239fc2
shawonahmed2020 <shawonahmed.net@gmail.com>
2019-04-28 11:44:45 +0600

    userclassmodify

--- GitExample/User.cs
+++ GitExample/User.cs
@@ -11,5 +11,7 @@ namespace GitExample
         public int  Id { get; set; }
         public string  Name { get; set; }
         public string Address { get; set; }
+        public string MobileNo { get; set; }
+        public string Details { get; set; }
     }
 }
